9638e9557bbb1deae06bfb9b601701c92e4a4173
Isaac Jennings <115528556+isaac-jennings@users.noreply.github.com>
2023-07-11 12:14:33 +1000

    Update AdaptiveGridReduction.py
    Add lambda function to wrangle CSV after input. Add KeyError exception handling to final sort.

--- src/AdaptiveGridReduction.py
+++ src/AdaptiveGridReduction.py
@@ -41,8 +41,10 @@ def adaptive_grid_culling(solutions, desired_size, num_divisions):
     normalize_objectives(solutions)
     calculate_grid_indices(solutions, num_divisions)
     density_map = calculate_grid_density(solutions, num_divisions)
-
-    solutions.sort(key=lambda x: (density_map[str(x['gridIndex'])], str(x['gridIndex'])))
+    try:
+        solutions.sort(key=lambda x: (density_map[str(x['gridIndex'])], str(x['gridIndex'])))
+    except KeyError:
+        pass
     return solutions[:desired_size]
 
 # Solutions = total number of current solutions before adapative grid reduction
@@ -50,3 +52,19 @@ def adaptive_grid_culling(solutions, desired_size, num_divisions):
 # Number of divisions = number of divisions/segements for each dimension. For example, three dimensions will equate to 1000
 # cubes. Density is irrelavant per cube, as some cubes may be empty.
 # Reduction will focus on the densest cube, repeating reduction until desired size is reached.
+
+import pandas as pd
+import csv
+df = pd.read_csv('summary_500.csv')
+df = df.set_index('objectives').apply(lambda row: {row.name: row.values.tolist()}, axis=1).tolist()
+culled = adaptive_grid_culling(df, 250, 50)
+
+# Specify the CSV file path
+csv_file_path = 'culled.csv'
+
+keys = culled[0].keys()
+
+with open(csv_file_path, 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, fieldnames=keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(culled)
